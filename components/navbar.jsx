fac4783063d82daf2fb97f27ba849617bf230fdc
mitchdmarino <mitchdmarino@gmail.com>
2022-07-25 12:48:07 -0700

    navbar fixes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
     <>
       <div className=''>
         <Disclosure >
-          <Disclosure.Button className="fixed  top-4 right-4 inline-flex items-center  justify-center rounded-md p-2 text-gray-800 hover:text-white  group">
+          <Disclosure.Button className="fixed  top-4 right-4 inline-flex items-center  justify-center rounded-md p-2 text-gray-800 hover:text-[#95190C]  group">
             <GiHamburgerMenu
               className="h-[40px] w-[40px]"
               aria-hidden="true"
@@ -51,48 +51,58 @@ export default function Navbar() {
                   </a>
                 </Disclosure.Button>
                 <div className=" my-4 border-b border-gray-100 pb-4">
-                  <Disclosure.Button className="w-full">
-                    <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                      <AiOutlineInfoCircle className="text-2xl text-white group-hover:text-white " />
-                        <a href="#about"className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
-                          About
-                        </a>
+                  <a href="#about">
+                    <Disclosure.Button className="w-full">
+                      <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                        <AiOutlineInfoCircle className="text-2xl text-white group-hover:text-white " />
+                          <p className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
+                            About
+                          </p>
+                      </div>
+                    </Disclosure.Button>
+                  </a>
+                  <a href="#skills">
+                    <Disclosure.Button className="w-full">
+                      <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                        <AiOutlineTool className="text-2xl text-white group-hover:text-white " />
+                          <p className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
+                            Skills
+                          </p>
+                      </div>
+                    </Disclosure.Button>
+                  </a>
+                  <a href="#projects">
+                    <Disclosure.Button className="w-full">
+                      <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                        <AiOutlineProject className="text-2xl text-white group-hover:text-white " />
+                          <p className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
+                            Projects
+                          </p>
+                      </div>
+                    </Disclosure.Button>
+                  </a>
+                  <a href="#contact">
+                    <Disclosure.Button className="w-full">
+                      <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                        <MdContactPage className="text-2xl text-white group-hover:text-white " />
+                          <p className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
+                            Contact
+                          </p>
+                      </div>
+                    </Disclosure.Button>
+                  </a>
+
+                  <a target="_blank" href='/Resume.pdf'  rel="noopener noreferrer" >
+                    <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                      <AiOutlineDownload className="text-2xl text-white group-hover:text-white " />
+                      <p className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white px-5 font-bold items-center justify-center  hover:text-[#F3FCF0]'>
+                        Resume
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                        </svg>
+                      </p>
                     </div>
-                  </Disclosure.Button>
-                  <Disclosure.Button className="w-full">
-                    <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                      <AiOutlineTool className="text-2xl text-white group-hover:text-white " />
-                        <a href="#skills"className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
-                          Skills
-                        </a>
-                    </div>
-                  </Disclosure.Button>
-                  <Disclosure.Button className="w-full">
-                    <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                      <AiOutlineProject className="text-2xl text-white group-hover:text-white " />
-                        <a href="#projects"className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
-                          Projects
-                        </a>
-                    </div>
-                  </Disclosure.Button>
-                  <Disclosure.Button className="w-full">
-                    <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                      <MdContactPage className="text-2xl text-white group-hover:text-white " />
-                        <a href="#contact"className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-left px-5 font-bold items-center justify-center  hover:text-[#F3FCF0] '>
-                          Contact
-                        </a>
-                    </div>
-                  </Disclosure.Button>
-                  
-                  <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                    <AiOutlineDownload className="text-2xl text-white group-hover:text-white " />
-                    <a target="_blank" href='/Resume.pdf'  rel="noopener noreferrer" className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white px-5 font-bold items-center justify-center  hover:text-[#F3FCF0]'>
-                      Resume
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                      </svg>
-                    </a>
-                  </div>
+                  </a>
                 </div>  
               </div>
             </Disclosure.Panel>
