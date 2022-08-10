2edf847f61fefc5f0c4116234258f540845670f1
mitchdmarino <mitchdmarino@gmail.com>
2022-08-10 16:11:03 -0700

    form complete

--- pages/projects/stratego.js
+++ pages/projects/stratego.js
@@ -1,13 +1,24 @@
-export default function Stratego() {
-    return (
-        <>
-            {/* title */}
-            <h1>Project Spotlight: Stratego</h1>
+import Link from "next/link";
 
-            {/* description */}
-            {/* approach */}
-            {/* hurdles */}
-            {/* future plans */}
-        </>
-    )
+export default function Stratego() {
+  return (
+    <>
+      <button>
+        <Link href="/#projects">Back</Link>
+      </button>
+      {/* title */}
+      <h1 className="">Project Spotlight: Stratego</h1>
+
+      {/* description */}
+      <p>
+        Stratego is a classic board game consisting of a battlefield where two
+        players each attempt to capture the opponent flag. My goal for this
+        project was to create a functional gameboard in HTML Canvas powered by
+        vanilla JavaScript.
+      </p>
+      {/* approach */}
+      {/* hurdles */}
+      {/* future plans */}
+    </>
+  );
 }
