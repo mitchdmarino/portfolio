8e5573fd77fb0a4f9c5055a5b3157f7fd7bc4074
mitchdmarino <mitchdmarino@gmail.com>
2022-08-30 12:15:27 -0700

    changes to skill icons on project cards

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,55 +1,21 @@
 import Image from 'next/image'
+import { projects } from '../utils/projects'
 
 export default function Projects() {
-    
-    const projects = [
-        {
-            name: 'Stratego',
-            description: 'Classic board game',
-            tools: ['JavaScript', 'HTML5', 'CSS', 'Canvas'],
-            image: '/Stratego.png',
-            github: 'https://github.com/mitchdmarino/Stratego-mock',
-            deployed: 'https://mitchdmarino.github.io/Stratego-mock/',
-        },
-        {
-            name: 'Simplifry',
-            description: 'Personal recipe library with nutritional info',
-            tools: ['Express', 'Node', 'SQL', 'sequelize', 'EJS Layouts'],
-            image: '/Simplifry.png',
-            github: 'https://github.com/mitchdmarino/Simplifry',
-            deployed: 'https://simplifry.herokuapp.com/'
-        },
-        {
-            name: 'CheckMate',
-            description: 'Organizational/Collaborative task list',
-            tools: ['Express', 'Node', 'Tailwind', 'React', 'MongoDB', 'Mongoose'],
-            image: '/CheckMate.png',
-            github: 'https://github.com/mitchdmarino/InCRUDibles-client',
-            deployed: 'https://dreamy-puffpuff-ae38ac.netlify.app/'
-        },
-        {
-            name: 'Flex',
-            description: 'Workout scheduler',
-            tools: ['Python','Django', 'Postgresql', 'Pytest', 'Bootstrap'],
-            image: '/Flex.png',
-            github: 'https://github.com/mitchdmarino/Flex',
-            deployed: 'na'
-        }
-      
-    ]
+
 
     const projCards = projects.map(project => {
         return (
-            <div key={project.name}className="max-w-[400px] p-4 mx-auto rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl bg-blue-300 m-10 ">
+            <div key={project.name}className="max-w-[400px] p-4 mx-auto rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl bg-blue-300 m-10 transition ease-in-out delay-100 duration-300 hover:bg-blue-200 hover:text-black">
                 
                 <div className="px-6 py-4">
                     
                     <div className="font-bold text-5xl mb-2  ">{project.name}</div>
                     
-                    <p className="text-gray-700 text-lg h-[40px] ">
+                    <p className=" text-2xl ">
                     {project.description}
                     </p>
-                    <div className='flex justify-center align-center rounded-xl'>
+                    <div className='flex justify-center my-5 align-center rounded-xl'>
                     <Image 
                         className='rounded-xl'
                         src={project.image}
@@ -58,20 +24,19 @@ export default function Projects() {
                         width={350}
                     />
                 </div>
-                   <div className="flex justify-center pt-10">
-                    <a className=" py-2 mx-4 block text-[#95190C] text-xl text-decoration-line: underline" href={project.github}>Repo</a>
-                    <br></br>
-                    <a className="py-2 block mx-4 text-[#95190C] text-xl text-decoration-line: underline" href={project.deployed}>Live</a>
-                    {/* <a className="py-2 block mx-4 text-[#95190C] text-xl text-decoration-line: underline" href={`projects/${project.name.toLowerCase()}`}>Case Study</a> */}
-                    </div>
-                </div>
-                <div className="px-6 pt-4 pb-2">
+                <div className="px-6 pt-4 pb-2 flex justify-center">
                     
-                    {project.tools.map(tool => {
+                    {project.tools.map((tool,i) => {
                         return (
-                            <span key={project.name + tool.name} className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tool}</span>
+                            <div key={`project${project.name}tool-${i}`} className='mb-3 mx-auto flex relative w-[40px] sm:w-[75px] md:w-[90px] lg:w-[110px] xl:w-[150px]'>
+                                    {tool}
+                            </div>
                         )
                     })}
+                </div>
+                   <div className="flex justify-center pt-10">
+                   <a className="py-2 block mx-4  text-xl text-decoration-line: underline" href={`projects/${project.name.toLowerCase()}`}>Learn More</a>                    {/* <a className="py-2 block mx-4 text-[#95190C] text-xl text-decoration-line: underline" href={`projects/${project.name.toLowerCase()}`}>Case Study</a> */}
+                    </div>
                 </div>
             </div>
         )
