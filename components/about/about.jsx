416bb97ea445f626adbb56a68b447529b1f81014
mitchdmarino <mitchdmarino@gmail.com>
2022-12-12 22:01:59 -0800

    About Section Updated

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Skills from '../skills'
+import Image from 'next/image'
 
 
 export default function About () {
@@ -11,20 +12,37 @@ export default function About () {
 
   return (
     <section id="about" className="w-full p-10 md:px-16 lg:px-20 pb-40">
-      <h1 className="text-6xl pb-10 text-center">ABOUT</h1>
-      <div className="flex flex-col justify-center">
+      <h1 className="text-6xl p-14 text-center">ABOUT</h1>
+      <div className="flex flex-col md:flex-row justify-center">
         <div className="w-full">
           <p className="text-2xl">
-            {"I'm"} a Full-Stack Software Engineer at <a href="https://www.roadieslabs.xyz" target="_blank" rel="noreferrer">Roadies Labs Inc.</a> 
+            {"I'm"} a Full-Stack Software Engineer at <a href="https://www.roadieslabs.xyz" className="highlight" target="_blank" rel="noreferrer">Roadies Labs Inc.</a> 
+            </p>
             <br></br>
-            <br></br>
-            {"I'm"} a self-starter {"who's"} passionate about creating user-oriented front-ends and organized, efficient back-ends.
-          </p>
-          <p className={details? "": "hidden" } >I started my professional career in the biotech industry, but made a drastic career change after finding my passion for software engineering. I find that many of my previous skills as a scientist (such as research, experimentation, and strong attention to detail) remain valuable assets to my professional success. I view coding through a scientific lens, which helps me work efficiently and methodically. </p>
-          <button onClick={() => handleDetailsClick()}>
-            {details ? "HIDE": "SEE MORE ->"}
+            <p className='text-xl max-w-[600px]'>
+            As a self-starter, I have passion for creating user-oriented front-ends and organized, efficient back-ends.
+            </p>
+          
+          <p className={details? "text-xl": "hidden"} >I started my professional career in the biotech industry, but made a drastic career change after finding my passion for software engineering. I find that many of my previous skills as a scientist (such as research, experimentation, and strong attention to detail) remain valuable assets to my professional success. I view coding through a scientific lens, which helps me work efficiently and methodically. </p>
+          <button className="highlight text-3xl py-10"onClick={() => handleDetailsClick()}>
+            {details ? "HIDE": 
+            "SEE MORE"}
           </button>
         </div>
+         <div className="profile h-[360px] w-[360px] pt-[5px] px-[5px] mx-auto  ">
+            <Image
+              className="profile"
+              src="/linkedinprofilepic.jpg"
+              alt="Mitchell Marino Profile Picture"
+              height={350}
+              width={350}
+            />
+            <style global jsx>{`
+              .profile {
+                border-radius: 10%;
+              }
+            `}</style>
+          </div>
       </div>
       <div id="skills">
         <Skills />
--- components/intro/intro.jsx
+++ components/intro/intro.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import Image from "next/image"
 import styles from "./intro.module.css"
 import useWindowDimensions from "../../utils/helpers/useWindowDimensions"
 
@@ -9,20 +8,6 @@ export default function Intro () {
     
     return (
         <section id="home" className="">
-          {/* <div className="bg-[#F8F0FB] profile h-[360px] w-[360px] pt-[5px]   px-[5px] mx-auto  ">
-            <Image
-              className="profile"
-              src="/linkedinprofilepic.jpg"
-              alt="Mitchell Marino Profile Picture"
-              height={350}
-              width={350}
-            />
-            <style global jsx>{`
-              .profile {
-                border-radius: 10%;
-              }
-            `}</style>
-          </div> */}
           <div className="w-full px-20" style={{paddingTop: height/3.5}}>
             <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl my-10 text-center">
               Hi there. {"I'm"} <span className={styles.nameTitle}>Mitchell Marino,</span>
